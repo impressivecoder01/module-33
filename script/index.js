1a059265d7c9089a5484df503e40567248b86a32
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-05 00:46:50 +0600

    class four added

--- script/index.js
+++ script/index.js
@@ -4,6 +4,27 @@ const loadLessons = ()=>{
     .then(json => showData(json.data))
 }
 
+const loadLevelWord = (id) =>{
+    // console.log(id)
+    fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+    .then(res => res.json())
+    .then(data => displayLabelWord(data.data))
+}
+
+const displayLabelWord = (words) =>{
+    // console.log(words)
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = '';
+
+    words.forEach(word=>{
+        const card = document.createElement('div')
+        card.innerHTML = `
+        <p>c</p>
+        `
+        wordContainer.appendChild(card)
+    })
+}
+
 const showData=(lessons)=>{
     // console.log(datas)
     const labelContainer = document.getElementById('label-container')
@@ -11,8 +32,7 @@ const showData=(lessons)=>{
     lessons.forEach(lesson=>{
         const btnDiv = document.createElement('div')
         btnDiv.innerHTML = `
-        <button class="flex items-center justify-center gap-1 btn btn-outline btn-primary"><i class="fa-brands fa-leanpub"></i><a
-        href="">Lesson</a></button>
+        <button onClick="loadLevelWord(${lesson['level_no']})" class="flex items-center justify-center gap-1 btn btn-outline btn-primary"><i class="fa-brands fa-leanpub"></i> Lesson-${lesson['level_no']}</button>
         `
         labelContainer.appendChild(btnDiv)
     })
